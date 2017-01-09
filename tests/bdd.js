b1b3bcc8c6b6c9d31b982e6ef8a484e36c59fc81
Olivier Bossel <olivier.bossel@gmail.com>
2017-01-09 08:08:13 +0100

    clean some things to make boilerplate more generic

--- tests/bdd.js
+++ tests/bdd.js
@@ -1,6 +1,6 @@
 import "babel-polyfill";
 import "webcomponents.js/webcomponents-lite";
-import SInteractiveDemoComponent from '../dist/index';
+import Component from '../dist/index';
 import { expect } from 'chai'
 import testingStack from 'coffeekraken-testing-stack';
 const html = require('./fixture.html');
@@ -11,16 +11,16 @@ testingStack.mocha.run();
 
 // tests
 describe('component', () => {
-	let component, originalContent;
-	before((done) => {
-		// grab the component
-		component = document.querySelector('component');
-		setTimeout(done,1000);
-	});
-	afterEach(() => {
-		component.onComponentDidUpdate = null;
-	});
-	it('Should be a cool component', () => {
+	// let component;
+	// before((done) => {
+	// 	// grab the component
+	// 	component = document.querySelector('s-google-map');
+	// 	setTimeout(done,1000);
+	// });
+	// afterEach(() => {
+	// 	component.onComponentDidUpdate = null;
+	// });
+	it('Is ok cause no tests have actually been written for now...', () => {
 		expect(true).to.equal(true);
 	});
 });
